4e20e3547dd12e7487dfd069e88d8c385e8ca755
BrunBoy <23rreddy@athenian.org>
2021-01-22 01:07:26 +0000

    done

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,11 @@ public class MyMain {
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
+        if (board[row][col]==true){
+            System.out.println("There was a hit!");
+            return true;
+        }
+        System.out.println("You missed a boat!");
         return false;
     }
 
@@ -13,7 +17,17 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        board[row][col]=true;
+        if (direction.equals("down")){
+            for (int i=0; i<boatLength; i++){
+                board[row+i][col]=true;
+            }
+        }
+        if (direction.equals("right")){
+            for (int i=0; i<boatLength; i++){
+                board[row][col+i]=true;
+            }
+        }
         return board;
     }
 
@@ -22,8 +36,18 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        boolean inOrder=true;
+        for (int row=0; row<words.length; row++){
+            for (int col=0; col<words[0].length-1; col++){
+                if (words[row][col].length()>words[row][col+1].length()){
+                    inOrder=false;
+                }
+                if (words[row][col].charAt(0)>words[row][col+1].charAt(0)){
+                    inOrder=false;
+                }
+            }
+        }
+        return inOrder;
     }
 
     public static void main(String[] args) {
